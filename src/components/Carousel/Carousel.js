4e158866f259c41b3a83069b08b57f26cd61d947
Ganesh Dalei <ganesh.dalei@kriritindia.com>
2024-02-16 06:41:02 -0800

    added-scroll-bar

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,5 +1,6 @@
 'use client'
 import { Carousel } from "@material-tailwind/react";
+import Card from '../card/SimpleCard';
 const url = "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=2700/layout-engine/2022-05/Group-33704.jpg"
 const CarouselCustomNavigation = () => {
     return (

5db8c58edaa085a184c48b604317a0f281e465a5
Ganesh Dalei <ganesh.dalei@kriritindia.com>
2024-02-16 06:47:02 -0800

    remove-arrow

--- src/components/container/Containers.js
+++ src/components/container/Containers.js
@@ -28,7 +28,7 @@ const Containers = () => {
 
 
             <Container>
-                <Carousel responsive={responsive}  itemClass="carousel-item-padding-40-px"
+                <Carousel responsive={responsive}   removeArrowOnDeviceType={["tablet", "mobile"]} itemClass="carousel-item-padding-40-px"
                 >
                     <SimpleCard name="English Oven Brown Bread" quantity="400 gr" marketPrice="60" ourPrice="50"
                         url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/images/products/full_screen/pro_18396.jpg?ts=1697722165" />

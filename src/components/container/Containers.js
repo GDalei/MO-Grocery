215bd6cfed3e32cb1e756db2e4bef25b03db7109
Ganesh Dalei <ganesh.dalei@kriritindia.com>
2024-02-25 03:49:39 -0800

    fixed-scroll-isues

--- src/components/container/Containers.js
+++ src/components/container/Containers.js
@@ -79,48 +79,153 @@ const Containers = () => {
         <>
             <Container className={style.containerWapper2}>Soft Drink</Container>
             <Container>
-            <Slider {...settings}
-            asNavFor={nav1}
-            ref={(slider) => setSlider1(slider)}
-        >
+                <Slider {...settings}
+                    asNavFor={nav1}
+                    ref={(slider) => setSlider1(slider)}
+                >
                     <div>
-                        <SimpleCard />
+                    <SimpleCard name="English Oven Brown Bread" quantity="400 gr" marketPrice="60" ourPrice="50"
+                        url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/images/products/full_screen/pro_18396.jpg?ts=1697722165" />
                     </div>
                     <div>
-                        <SimpleCard />
+                    <SimpleCard name="Amul Fresh Malai Paneer" quantity="200 gr" marketPrice="91" ourPrice="91"
+                        url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/images/products/full_screen/pro_329500.jpg?ts=1684835682" />
+                   </div>
+                   <div>
+                    <SimpleCard name="Amul Salted Butter" quantity="100 gr" marketPrice="58" ourPrice="58"
+                        url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/assets/products/large_images/jpeg/840fefe7-87d8-4fe2-9c8f-214a7e16d395.jpg?ts=1707312315" />
                     </div>
                     <div>
-                        <SimpleCard />
+                    
+                    <SimpleCard url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/11150a.jpg?ts=1688463551" />
                     </div>
                     <div>
-                        <SimpleCard />
+                    <SimpleCard url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/57435a.jpg?ts=1688627217" />
                     </div>
                     <div>
-                        <SimpleCard />
+                    <SimpleCard url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/19512a.jpg?ts=1700652991" />
                     </div>
                     <div>
-                        <SimpleCard />
+                    <SimpleCard url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/full_screen/pro_540080.jpg?ts=1703665638" />
                     </div>
                     <div>
-                        <SimpleCard />
+                    <SimpleCard name="Amul Masti Curd" quantity="1 Kg" marketPrice="72" ourPrice="27"
+                        url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/images/products/full_screen/pro_45533.jpg?ts=1685975152" />
                     </div>
                     <div>
-                        <SimpleCard />
+                    <SimpleCard name="Amul Pure Milk Cheese Slices" quantity="200 gr" marketPrice="140" ourPrice="136"
+                        url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/assets/products/large_images/jpeg/32e1667a-295c-4e73-8492-4e20643c3dce.jpg?ts=1707312315" />
                     </div>
                     <div>
-                        <SimpleCard />
+                    <SimpleCard />
                     </div>
                     <div>
-                        <SimpleCard />
+                    <SimpleCard />
                     </div>
                     <div>
-                        <SimpleCard />
+                    <SimpleCard />
+                    </div>
+                </Slider>
+
+            </Container>
+            <Container className={style.containerWapper2}>Soft Drink</Container>
+            <Container>
+                <Slider {...settings}
+                    asNavFor={nav1}
+                    ref={(slider) => setSlider1(slider)}
+                >
+                    <div>
+                    <SimpleCard name="English Oven Brown Bread" quantity="400 gr" marketPrice="60" ourPrice="50"
+                        url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/images/products/full_screen/pro_18396.jpg?ts=1697722165" />
                     </div>
                     <div>
-                        <SimpleCard />
+                    <SimpleCard name="Amul Fresh Malai Paneer" quantity="200 gr" marketPrice="91" ourPrice="91"
+                        url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/images/products/full_screen/pro_329500.jpg?ts=1684835682" />
+                   </div>
+                   <div>
+                    <SimpleCard name="Amul Salted Butter" quantity="100 gr" marketPrice="58" ourPrice="58"
+                        url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/assets/products/large_images/jpeg/840fefe7-87d8-4fe2-9c8f-214a7e16d395.jpg?ts=1707312315" />
                     </div>
                     <div>
-                        <SimpleCard />
+                    
+                    <SimpleCard url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/11150a.jpg?ts=1688463551" />
+                    </div>
+                    <div>
+                    <SimpleCard url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/57435a.jpg?ts=1688627217" />
+                    </div>
+                    <div>
+                    <SimpleCard url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/19512a.jpg?ts=1700652991" />
+                    </div>
+                    <div>
+                    <SimpleCard url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/full_screen/pro_540080.jpg?ts=1703665638" />
+                    </div>
+                    <div>
+                    <SimpleCard name="Amul Masti Curd" quantity="1 Kg" marketPrice="72" ourPrice="27"
+                        url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/images/products/full_screen/pro_45533.jpg?ts=1685975152" />
+                    </div>
+                    <div>
+                    <SimpleCard name="Amul Pure Milk Cheese Slices" quantity="200 gr" marketPrice="140" ourPrice="136"
+                        url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/assets/products/large_images/jpeg/32e1667a-295c-4e73-8492-4e20643c3dce.jpg?ts=1707312315" />
+                    </div>
+                    <div>
+                    <SimpleCard />
+                    </div>
+                    <div>
+                    <SimpleCard />
+                    </div>
+                    <div>
+                    <SimpleCard />
+                    </div>
+                </Slider>
+
+            </Container>
+            <Container className={style.containerWapper2}>Soft Drink</Container>
+            <Container>
+                <Slider {...settings}
+                    asNavFor={nav1}
+                    ref={(slider) => setSlider1(slider)}
+                >
+                    <div>
+                    <SimpleCard name="English Oven Brown Bread" quantity="400 gr" marketPrice="60" ourPrice="50"
+                        url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/images/products/full_screen/pro_18396.jpg?ts=1697722165" />
+                    </div>
+                    <div>
+                    <SimpleCard name="Amul Fresh Malai Paneer" quantity="200 gr" marketPrice="91" ourPrice="91"
+                        url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/images/products/full_screen/pro_329500.jpg?ts=1684835682" />
+                   </div>
+                   <div>
+                    <SimpleCard name="Amul Salted Butter" quantity="100 gr" marketPrice="58" ourPrice="58"
+                        url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/assets/products/large_images/jpeg/840fefe7-87d8-4fe2-9c8f-214a7e16d395.jpg?ts=1707312315" />
+                    </div>
+                    <div>
+                    
+                    <SimpleCard url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/11150a.jpg?ts=1688463551" />
+                    </div>
+                    <div>
+                    <SimpleCard url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/57435a.jpg?ts=1688627217" />
+                    </div>
+                    <div>
+                    <SimpleCard url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/19512a.jpg?ts=1700652991" />
+                    </div>
+                    <div>
+                    <SimpleCard url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/full_screen/pro_540080.jpg?ts=1703665638" />
+                    </div>
+                    <div>
+                    <SimpleCard name="Amul Masti Curd" quantity="1 Kg" marketPrice="72" ourPrice="27"
+                        url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/images/products/full_screen/pro_45533.jpg?ts=1685975152" />
+                    </div>
+                    <div>
+                    <SimpleCard name="Amul Pure Milk Cheese Slices" quantity="200 gr" marketPrice="140" ourPrice="136"
+                        url="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/assets/products/large_images/jpeg/32e1667a-295c-4e73-8492-4e20643c3dce.jpg?ts=1707312315" />
+                    </div>
+                    <div>
+                    <SimpleCard />
+                    </div>
+                    <div>
+                    <SimpleCard />
+                    </div>
+                    <div>
+                    <SimpleCard />
                     </div>
                 </Slider>
 

f87dd184dcf9bd8e71ffc985496dc9c538438f03
Ganesh Dalei <ganesh.dalei@kriritindia.com>
2024-02-25 01:19:20 -0800

    fixed-scroll-issue

--- src/components/adminComponents/TabPanel/TabPanelComponent.js
+++ src/components/adminComponents/TabPanel/TabPanelComponent.js
@@ -1,92 +1,62 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import TableComponent from '../TableComponent';
-
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-
+import {
+    Tabs,
+    TabsHeader,
+    TabsBody,
+    Tab,
+    TabPanel,
+  } from "@material-tailwind/react";
+   import TableComponent from '../TableComponent';
+  export default function TabPanelComponent() {
+    const data = [
+      {
+        label: "HTML",
+        value: <TableComponent/>,
+        desc: <TableComponent/>,
+      },
+      {
+        label: "React",
+        value: "react",
+        desc: `Because it's about motivating the doers. Because I'm here
+        to follow my dreams and inspire other people to follow their dreams, too.`,
+      },
+      {
+        label: "Vue",
+        value: "vue",
+        desc: `We're not always in the position that we want to be at.
+        We're constantly growing. We're constantly making mistakes. We're
+        constantly trying to express ourselves and actualize our dreams.`,
+      },
+      {
+        label: "Angular",
+        value: "angular",
+        desc: `Because it's about motivating the doers. Because I'm here
+        to follow my dreams and inspire other people to follow their dreams, too.`,
+      },
+      {
+        label: "Svelte",
+        value: "svelte",
+        desc: `We're not always in the position that we want to be at.
+        We're constantly growing. We're constantly making mistakes. We're
+        constantly trying to express ourselves and actualize our dreams.`,
+      },
+    ];
+   
     return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`vertical-tabpanel-${index}`}
-            aria-labelledby={`vertical-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box sx={{ p: 3 }}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
+      <Tabs value="html" >
+        <TabsHeader className="w-32">
+          {data.map(({ label, value }) => (
+            <Tab key={value} value={value}>
+              {label}
+            </Tab>
+          ))}
+        </TabsHeader>
+        <TabsBody>
+          {data.map(({ value, desc }) => (
+            <TabPanel key={value} value={value} className="py-0">
+              {desc}
+            </TabPanel>
+          ))}
+        </TabsBody>
+      </Tabs>
     );
-}
-
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-    return {
-        id: `vertical-tab-${index}`,
-        'aria-controls': `vertical-tabpanel-${index}`,
-    };
-}
-
-export default function TabPanelComponent() {
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-
-    return (
-        <Box
-            sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex' }}
-        >
-            <Tabs
-                orientation="vertical"
-                variant="scrollable"
-                value={value}
-                onChange={handleChange}
-                aria-label="Vertical tabs example"
-                sx={{ borderRight: 1, borderColor: 'divider' }}
-            >
-                <Tab label="Item One" {...a11yProps(0)} />
-                <Tab label="Item Two" {...a11yProps(1)} />
-                <Tab label="Item Three" {...a11yProps(2)} />
-                <Tab label="Item Four" {...a11yProps(3)} />
-                <Tab label="Item Five" {...a11yProps(4)} />
-                <Tab label="Item Six" {...a11yProps(5)} />
-                <Tab label="Item Seven" {...a11yProps(6)} />
-            </Tabs>
-            <TabPanel value={value} index={0}>
-gugu
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-                Item Two
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-                Item Three
-            </TabPanel>
-            <TabPanel value={value} index={3}>
-                Item Four
-            </TabPanel>
-            <TabPanel value={value} index={4}>
-                Item Five
-            </TabPanel>
-            <TabPanel value={value} index={5}>
-                Item Six
-            </TabPanel>
-            <TabPanel value={value} index={6}>
-                Item Seven
-            </TabPanel>
-        </Box>
-    );
-}
+  }
